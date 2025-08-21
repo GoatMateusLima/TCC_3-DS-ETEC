673d75fc55294724ccd9300bdf180766ba6d3636
Mateus lima Bispo <mateuslimabispo15@gmail.com>
2025-08-20 22:27:03 -0300

    criando conexao a api

---
+++ Aumigos/front-end/src/pages/login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="stylesheet" href="/TCC_3-DS-ETEC-1/Aumigos/front-end/src/css/pages/login.css">
+
+    <link rel="stylesheet" href="/TCC_3-DS-ETEC-1/Aumigos/front-end/src/css/reset.css">
+    <link rel="stylesheet" href="/TCC_3-DS-ETEC-1/Aumigos/front-end/src/css/globais.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Ribeye&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <figure class="logo">
+            <img src="/TCC_3-DS-ETEC-1/Aumigos/front-end/src/assets/img/2.webp" alt="logo">
+        </figure>
+        <nav class="navegacao">
+
+            <a href="about.html"> Ajuda</a>
+            <a href="ongs_parceiras.html">ONGs</a>
+            <a href="dashboard.html">Quero adotar</a>
+            <a href="account_settings.html">Conta</a>
+
+        </nav>
+    </header>
+
+
+    <main>
+        <section class="section">
+            <figure class="imgGatinho">
+                <img src="/TCC_3-DS-ETEC-1/Aumigos/front-end/src/assets/img/imgGatinho.webp" alt="gatinho">
+                <figcaption></figcaption>
+            </figure>
+            <form action="" class="form">
+                <picture>
+                    <img src="/TCC_3-DS-ETEC-1/Aumigos/front-end/src/assets/img/login título.png" alt=" titulo">
+                </picture>
+                <label class="form-label">
+                    <input type="email" id="email" required placeholder=" " />
+                    <span>Email</span>
+                </label>
+                <label class="form-label">
+                    <input type="password" id="senha" required placeholder=" " />
+                    <span>Senha</span>
+                </label>
+                <button class="entrar">Entrar</button>
+                <nav class="ajuda">
+                    <a href="">Esqueci minha senha</a>
+                    <a href="cadastro.html">Não tem cadastro?Clique aqui.</a>
+                </nav>
+            </form>
+        </section>
+    </main>
+    <footer>
+        askdakskdaskkaskdk
+    </footer>
+</body>
+
+</html>
