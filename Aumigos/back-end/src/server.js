f11323ab222390ad9f6ae4b686a09825fe8434ed
GoatMateusLima <mateuslimabispo15@gmail.com>
2025-09-14 23:38:01 -0300

    testando conexao cnpjroutes e server

---
+++ Aumigos/back-end/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cnpjRoutes = require('./routes/cnpjRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/cnpj', cnpjRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () =>{
+    console.log(`Server executando na ${PORT}`);
+});
