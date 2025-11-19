26069e85b37be645616a08f63865ac628b8e25e8
Mateus lima Bispo <mateuslimabispo15@gmail.com>
2025-11-18 23:25:44 -0300

    oal

--- Aumigos/front-end/src/js/global/verificacao_login.js
+++ Aumigos/front-end/src/js/global/verificacao_login.js
@@ -2,27 +2,27 @@ function verificarConta() {
     const usuario = JSON.parse(localStorage.getItem("usuarioAtual"));
 
     // Se não tem ninguém logado → login
+
     if (!usuario || !usuario.tipo) {
-        window.location.href = "http://localhost:5050/front-end/src/pages/login.html";
+        window.location.href = "/src/pages/login.html";
         return;
     }
 
-    // Redireciona para a página do tipo
     switch (usuario.tipo) {
         case "adotante":
-            window.location.href = "http://localhost:5050/front-end/src/pages/user/adotante.html";
+            window.location.href = "/src/pages/user/adotante.html";
             break;
 
         case "ong":
-            window.location.href = "http://localhost:5050/front-end/src/pages/user/ong.html";
+            window.location.href = "/src/pages/user/ong.html";
             break;
 
         case "membro":
-            window.location.href = "http://localhost:5050/front-end/src/pages/user/membro_ong.html";
+            window.location.href = "/src/pages/user/membro_ong.html";
             break;
 
         default:
-            window.location.href = "http://localhost:5050/front-end/src/pages/login.html";
+            window.location.href = "/src/pages/login.html";
             break;
     }
 }
