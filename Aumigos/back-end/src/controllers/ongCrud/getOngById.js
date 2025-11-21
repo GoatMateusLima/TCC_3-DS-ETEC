6f40278ed469feb4d699d4e6d1273935d48ed0af
Mateus lima Bispo <mateuslimabispo15@gmail.com>
2025-11-21 04:27:50 -0300

    config

--- Aumigos/back-end/src/controllers/ongCrud/getOngById.js
+++ Aumigos/back-end/src/controllers/ongCrud/getOngById.js
@@ -1,17 +1,21 @@
-// controllers/ongCrud/getOngById.js
 const db = require('../../config/dbClient');
 
 async function getOngById(req, res) {
     try {
-        const { id } = req.params;
-        if (!id) return res.status(400).json({ error: 'ID da ONG é obrigatório.' });
+        const { id } = req.params; // pega o id da rota /ongs/:id
+        if (!id) return res.status(400).json({ error: 'ID da ONG não fornecido.' });
 
-        const { data, error } = await db.from('ong').select('*').eq('id', id).single();
+        const { data, error } = await db
+            .from('ong')
+            .select('*')
+            .eq('ong_id', parseInt(id))
+            .single(); // pega apenas 1 registro
 
         if (error) return res.status(500).json({ error: 'Erro ao buscar ONG.', details: error });
         if (!data) return res.status(404).json({ error: 'ONG não encontrada.' });
 
         res.status(200).json(data);
+
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Erro interno ao buscar ONG.' });
